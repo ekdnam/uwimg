38fc02ed5a8fbfdb8ae4a474a708e64bd46d00f1
ekdnam <adeetya.m@gmail.com>
2020-11-30 13:20:32 +0530

    start

--- src/hw0/process_image.c
+++ src/hw0/process_image.c
@@ -4,15 +4,64 @@
 #include <math.h>
 #include "image.h"
 
+    /**
+     * Returns the clamped pixel coordinates
+    * Args:
+    *   1. im: struct containing the image data in the form of a float matrix
+    *   2. x: int value which specifies the x coordinate of the pixel
+    *   3. y: int value which specifies the y coordinate of the pixel
+    *   4. c: int value which specifies the #channel
+    * returns:
+    *   pointer to an int arrray having the clamped values
+    */
+int* return_clamped_values(image im, int x, int y, int c){
+
+    int correct_pos[3];
+    if(x < 0){
+        correct_pos[0] = 0;
+    }
+    if(x > im.w){
+        correct_pos[0] = im.w - 1;
+    }
+    if(y < 0){
+        correct_pos[1] = 0;
+    }
+    if(y > im.h){
+        correct_pos[1] = im.h - 1;
+    }
+    if(c < 0){
+        correct_pos[2] = 0;
+    }
+    if(c > im.c){
+        correct_pos[2] = im.c - 1;
+    }
+    return correct_pos;
+}
+
 float get_pixel(image im, int x, int y, int c)
 {
     // TODO Fill this in
-    return 0;
+    int *xyz;
+    xyz = return_clamped_values(im, x, y, c);
+    x = xyz[0];
+    y = xyz[1];
+    c = xyz[2];
+    // as matrices are stored in the row-major for in C
+    // we are accessing individual elements of image in a similar fashion
+    float pixel = im.data[x + y*im.w + c*im.h*im.w];
+    return pixel;
 }
 
 void set_pixel(image im, int x, int y, int c, float v)
 {
     // TODO Fill this in
+    int *xyz;
+    xyz = return_clamped_values(im, x, y, c);
+    x = xyz[0];
+    y = xyz[1];
+    c = xyz[2];
+    im.data[x + y*im.w + c*im.h*im.w] = v;
+
 }
 
 image copy_image(image im)
